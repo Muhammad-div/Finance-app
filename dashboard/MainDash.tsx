b8c999d68856f8d5867a5bbda7e602f762373e85
hashirkn <hashir.carbonicit@gmail.com>
2023-11-06 17:59:49 +0500

    updated

--- dashboard/MainDash.tsx
+++ dashboard/MainDash.tsx
@@ -1,17 +1,21 @@
 import { Box } from "@mui/material";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
-import { useMemo } from "react";
+import { useMemo, useEffect } from "react";
 import { themeSettings } from "./theme";
 import Navbar from "./scenes/navbar";
-// import Navbar from "@/scenes/navbar";
 import Dashboard from "./scenes/dashboard";
-// import Predictions from "./scenes/predictions";
 import { Provider } from "react-redux";
-// import { api } from "./state/api";
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { api } from "./state/api";
+import { useRouter } from "next/router"; // Import the useRouter hook from Next.js
+
+// Function to check if the token is available in local storage
+const isTokenAvailable = () => {
+  const token = localStorage.getItem("token");
+  return !!token; // Return true if token exists, false otherwise
+};
 
 export const store = configureStore({
   reducer: { [api.reducerPath]: api.reducer },
@@ -20,7 +24,20 @@ export const store = configureStore({
 setupListeners(store.dispatch);
 
 function MainDash() {
-  const theme = useMemo(() => createTheme(themeSettings), []);
+  const theme = useMemo(() => createTheme(themeSettings));
+  const router = useRouter(); // Initialize the router
+
+  // Check if the token is available in local storage
+  const tokenAvailable = isTokenAvailable();
+
+  // Use useEffect to handle the redirection
+  useEffect(() => {
+    if (!tokenAvailable) {
+      // Redirect to the /login page
+      router.push("/login");
+    }
+  }, [tokenAvailable, router]);
+
   return (
     <div className="app" style={{ marginTop: "100px" }}>
       <Provider store={store}>
